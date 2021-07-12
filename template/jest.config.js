95ff535cb705df568d13f7573e177c16a81a8e32
diegom <diego@smartbr.com>
2021-07-11 21:22:24 -0300

    fix: correctly jest config ts jest

--- template/jest.config.js
+++ template/jest.config.js
@@ -1,15 +1,12 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.paths.json');
+
 module.exports = {
   preset: 'ts-jest',
   moduleFileExtensions: ['js', 'ts', 'tsx'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '^@test': '<rootDir>/src/test-helpers/index',
-    '^@components/(.*)$': '<rootDir>/src/components/$1',
-    '^@prodivers/(.*)$': '<rootDir>/src/providers/$1',
-    '^@config/(.*)$': '<rootDir>/src/configurations/$1',
-    '^@reducers/(.*)$': '<rootDir>/src/redux/reducers/$1',
-    '^@sagas/(.*)$': '<rootDir>/src/redux/sagas/$1',
-    '^@styles/(.*)$': '<rootDir>/src/common-styles/$1',
-  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
 };
--- template/src/components/component-a/component-a.test.tsx
+++ template/src/components/component-a/component-a.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render, screen } from '@test';
+import { render, screen } from '@testing-library/react';
 
 import ComponentA from './component-a';
 
